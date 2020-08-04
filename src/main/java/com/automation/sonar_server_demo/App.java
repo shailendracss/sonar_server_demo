516fab16907b7a7af313d202e9e99e14c1b746ff
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-04 11:20:56 +0530

    4 Aug sonardemo

--- src/main/java/com/automation/sonar_server_demo/App.java
+++ src/main/java/com/automation/sonar_server_demo/App.java
@@ -6,8 +6,9 @@ package com.automation.sonar_server_demo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+        Thread.sleep(2);
     }
 }
