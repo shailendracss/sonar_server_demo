93541ec29e1b68d635e9fb0de9f432674abe5f11
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-12 13:16:39 +0530

    Push Sonarserver-1:16 pm

---
+++ src/main/java/com/automation/sonar_server_demo/DemoClass.java
@@ -0,0 +1,55 @@
+package com.automation.sonar_server_demo;
+
+public class DemoClass {
+
+		private String name;
+		private int rollno;
+
+		public void SetDeta(String s, int i) {
+			this.name = s;
+			this.rollno = i;
+		}
+
+		public void display() {
+			System.out.println("name is   " + name + "   rollno is   " + rollno);
+		}
+
+		public void display_ssr() {
+			System.out.println("name is   " + name + "   rollno is   " + rollno);
+
+			int i = 10;
+			int j = i + 10;
+			int k = j + 10;
+			int l = k + 10;
+			int m = l + 10;
+			int n = m + 10;
+			int o = n + 10;
+			int p = o + 10;
+			int q = p + 10;
+			int r = q + 10;
+			int s = r + 10;
+			int t = s + 10;
+			int u = t + 10;
+
+		}
+
+		public void display_ssr1() {
+			System.out.println("name is   " + name + "   rollno is   " + rollno);
+
+			int i = 10;
+			int j = i + 10;
+			int k = j + 10;
+			int l = k + 10;
+			int m = l + 10;
+			int n = m + 10;
+			int o = n + 10;
+			int p = o + 10;
+			int q = p + 10;
+			int r = q + 10;
+			//int s = r + 10;
+
+		}
+
+	}
+
+
--- src/test/java/com/automation/sonar_server_demo/AppTest.java
+++ src/test/java/com/automation/sonar_server_demo/AppTest.java
@@ -1,38 +1,15 @@
 package com.automation.sonar_server_demo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void e1() {
+		DemoClass obj = new DemoClass();
+		// obj.display();
+		// obj.SetDeta(null, 0);
+		obj.display_ssr();
+		obj.display_ssr1();
+	}
 }
