9f327005b2f45b501afd3d6a57994783282fe19d
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-12 15:06:25 +0530

    Push Sonar Java error

--- src/main/java/com/automation/sonar_server_demo/DemoClass.java
+++ src/main/java/com/automation/sonar_server_demo/DemoClass.java
@@ -2,70 +2,77 @@ package com.automation.sonar_server_demo;
 
 public class DemoClass {
 
-		private String name;
-		private int rollno;
+	private String name;
+	private int rollno;
 
-		public void SetDeta(String s, int i) {
-			this.name = s;
-			this.rollno = i;
-		}
+	public void SetDeta(String s, int i) {
+		this.name = s;
+		this.rollno = i;
+	}
 
-		public void display() {
-			System.out.println("name is   " + name + "   rollno is   " + rollno);
-		}
+	public void display() {
+		System.out.println("name is   " + name + "   rollno is   " + rollno);
+	}
 
-		public void display_ssr() {
-			System.out.println("name is   " + name + "   rollno is   " + rollno);
+	public void display_ssr() {
+		System.out.println("name is   " + name + "   rollno is   " + rollno);
 
-			int i = 10;
-			int j = i + 10;
-			int k = j + 10;
-			int l = k + 10;
-			int m = l + 10;
-			int n = m + 10;
-			int o = n + 10;
-			int p = o + 10;
-			int q = p + 10;
-			int r = q + 10;
-			int s = r + 10;
-			int t = s + 10;
-			int u = t + 10;
-
-		}
-
-		public void display_ssr1() {
-			System.out.println("name is   " + name + "   rollno is   " + rollno);
-
-			int i = 10;
-			int j = i + 10;
-			int k = j + 10;
-			int l = k + 10;
-			int m = l + 10;
-			int n = m + 10;
-			int o = n + 10;
-			int p = o + 10;
-			int q = p + 10;
-			int r = q + 10;
-			//int s = r + 10;
-
-		}
-		public void display_ssr2() {
-			System.out.println("name is   " + name + "   rollno is   " + rollno);
-
-			int i = 10;
-			int j = i + 10;
-			int k = j + 10;
-			int l = k + 10;
-			int m = l + 10;
-			int n = m + 10;
-			int o = n + 10;
-			int p = o + 10;
-			int q = p + 10;
-			int r = q + 10;
-			//int s = r + 10;
-
-		}
+		int i = 10;
+		int j = i + 10;
+		int k = j + 10;
+		int l = k + 10;
+		int m = l + 10;
+		int n = m + 10;
+		int o = n + 10;
+		int p = o + 10;
+		int q = p + 10;
+		int r = q + 10;
+		int s = r + 10;
+		int t = s + 10;
+		int u = t + 10;
 
 	}
 
+	public void display_ssr1() {
+		System.out.println("name is   " + name + "   rollno is   " + rollno);
 
+		int i = 10;
+		int j = i + 10;
+		int k = j + 10;
+		int l = k + 10;
+		int m = l + 10;
+		int n = m + 10;
+		int o = n + 10;
+		int p = o + 10;
+		int q = p + 10;
+		int r = q + 10;
+		// int s = r + 10;
+
+	}
+
+	public void display_ssr2() {
+		System.out.println("name is   " + name + "   rollno is   " + rollno);
+
+		int i = 10;
+		int j = i + 10;
+		int k = j + 10;
+		int l = k + 10;
+		int m = l + 10;
+		int n = m + 10;
+		int o = n + 10;
+		int p = o + 10;
+		int q = p + 10;
+		int r = q + 10;
+		// int s = r + 10;
+
+	}
+
+	public void Demo() {
+		int target = -5;
+		int num = 3;
+
+		target = -num; // Noncompliant; target = -3. Is that really what's meant?
+		target = +num; // Noncompliant; target = 3
+	}
+
+}
