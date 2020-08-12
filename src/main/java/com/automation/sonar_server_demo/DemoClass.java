2d630efac65fa507f1f58386c7ad8172b3ab385b
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-12 17:50:28 +0530

    ghd

--- src/main/java/com/automation/sonar_server_demo/DemoClass.java
+++ src/main/java/com/automation/sonar_server_demo/DemoClass.java
@@ -72,7 +72,7 @@ public class DemoClass {
 		int num = 3;
 
 		target = -num; // Noncompliant; target = -3. Is that really what's meant?
-		target = +num; // Noncompliant; target = 3
+		target = +num; // Noncompliant; target = 33
 	}
 
 }
