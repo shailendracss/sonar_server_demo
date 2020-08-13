b0f8446e17d312070221affe46228d0eb1ae5a47
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-13 12:00:05 +0530

    gfj

--- src/main/java/com/automation/sonar_server_demo/ChildClass.java
+++ src/main/java/com/automation/sonar_server_demo/ChildClass.java
@@ -1,8 +1,8 @@
 package com.automation.sonar_server_demo;
 
-public class ChildClass extends ParentClass{
+public class ChildClass extends ParentClass {
 	public void doSomething() {
 		System.out.println("");
 	}
-	
+
 }
