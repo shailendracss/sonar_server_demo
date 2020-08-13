9bdad5674bc4b4ae461e46adeb6d512fcb3cd8ea
shailendracss <shailendra.rajawat@cssindiaonline.com>
2020-08-13 10:42:38 +0530

    @Ocverride bug active inactive test

---
+++ src/main/java/com/automation/sonar_server_demo/ChildClass.java
@@ -0,0 +1,9 @@
+package com.automation.sonar_server_demo;
+
+public class ChildClass extends ParentClass{
+	
+	public void doSomething() {
+		System.out.println("");
+	}
+	
+}
---
+++ src/main/java/com/automation/sonar_server_demo/ParentClass.java
@@ -0,0 +1,9 @@
+package com.automation.sonar_server_demo;
+
+public class ParentClass {
+	
+	public void doSomething() {
+		System.out.println("");
+	}
+	
+}
