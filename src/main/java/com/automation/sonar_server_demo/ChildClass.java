4658c6f44f7a653ba490c4d66df000633f32aab3
adarsh12345 <adarsh12345@adarsh12345-PC>
2020-08-20 14:52:13 +0530

    Push Overdide

--- src/main/java/com/automation/sonar_server_demo/ChildClass.java
+++ src/main/java/com/automation/sonar_server_demo/ChildClass.java
@@ -1,6 +1,8 @@
 package com.automation.sonar_server_demo;
 
 public class ChildClass extends ParentClass {
+	
+	@Override
 	public void doSomething() {
 		System.out.println("");
 	}
--- src/main/java/com/automation/sonar_server_demo/ParentClass.java
+++ src/main/java/com/automation/sonar_server_demo/ParentClass.java
@@ -1,6 +1,8 @@
 package com.automation.sonar_server_demo;
 
+
 public class ParentClass {
+	
 	
 	public void doSomething() {
 		System.out.println("");
